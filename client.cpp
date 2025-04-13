39a5e176971cdadcd3551c99b25ea19af897cc37
Victoria Leshchenko <victorialeshchenko7@gmail.com>
2025-04-13 22:44:01 +0300

    Implement matrix generation and preparation for data transfer in TCP client

--- client.cpp
+++ client.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
@@ -7,8 +10,73 @@
 #define PORT 1234
 #define SERVER_IP "127.0.0.1"
 
+#define MIN 1
+#define MAX 100
+
+enum ClientRequest
+{
+    CONFIG = 1,
+    CALCULATE,
+    GET_RESULT
+};
+
+struct MatrixData
+{
+    int matrixSize;
+    int threadsCount;
+    std::vector<std::vector<int>> matrix;
+
+    MatrixData(): matrixSize(0), threadsCount(0), matrix() {}
+
+    MatrixData(const int matrixSize_, const int threadsCount_): matrixSize(matrixSize_), threadsCount(threadsCount_), matrix(matrixSize_, std::vector<int>(matrixSize_))
+    {
+        fillMatrix();
+    }
+
+    void printMatrix()
+    {
+        for (const auto& row : matrix)
+        {
+            for (const auto val : row)
+            {
+                std::cout << val << "\t";
+            }
+            std::cout << "\n";
+        }
+    }
+
+private:
+    static int getRandomNumber()
+    {
+        return MIN + (rand() % (MAX - MIN + 1));
+    }
+
+    void fillMatrix()
+    {
+        for (int i = 0; i < matrixSize; ++i)
+        {
+            for (int j = 0; j < matrixSize; ++j)
+            {
+                matrix[i][j] = getRandomNumber();
+            }
+        }
+    }
+};
+
+void sendData()
+{
+
+}
+
+void receiveData()
+{
+
+}
+
 int main()
 {
+    srand(time(NULL));
+
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0)
     {
@@ -41,6 +109,16 @@ int main()
         WSACleanup();
         return 1;
     }
+
+    int matrixSize, threadsCount;
+    std::cout << "Enter matrix size: ";
+    std::cin >> matrixSize;
+
+    std::cout << "Enter number of threads: ";
+    std::cin >> threadsCount;
+
+    MatrixData data(matrixSize, threadsCount);
+    data.printMatrix();
 
     closesocket(clientSocket);
     WSACleanup();
